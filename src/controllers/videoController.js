490dd63f5c40a0c98c21ba1dabf740019e4fcf8d
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-05-09 23:47:36 +0900

    homework 4

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,9 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render('home', { pageTitle: 'Home' })
 export const see = (req, res) => {
-  return res.render("watch")
+  return res.render('watch', { pageTitle: 'Watch Video' })
 }
-export const edit = (req, res) => res.render("edit")
+export const edit = (req, res) =>
+  res.render('edit', { pageTitle: 'Edit Video' })
 export const search = (req, res) => res.send('Search')
 export const upload = (req, res) => res.send('upload')
 export const deleteVideo = (req, res) => res.send('Delete Video')
