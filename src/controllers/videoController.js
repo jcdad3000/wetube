39a6ce801e0433ad10ac0149e39ada61bf03c4fa
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-05-09 19:09:55 +0900

    lec 5.2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,8 @@
-export const trending = (req, res) => res.send('Home Page Videos')
+export const trending = (req, res) => res.render("home");
 export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`)
+  return res.render("watch")
 }
-export const edit = (req, res) => res.send('Edit')
+export const edit = (req, res) => res.render("edit")
 export const search = (req, res) => res.send('Search')
 export const upload = (req, res) => res.send('upload')
 export const deleteVideo = (req, res) => res.send('Delete Video')
