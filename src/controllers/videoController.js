2b6a495fb2611e8f87ecff7fd2f852b488e31887
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-05-11 14:55:39 +0900

    lec 6.0

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,38 +1,40 @@
-
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title:"First Video",
-      rating:5,
-      comments:2,
-      createdAt:"2 minutes ago",
-      views:59,
-      id:1,
-    },
-    {
-      title:"Second Video",
-      rating:5,
-      comments:2,
-      createdAt:"2 minutes ago",
-      views:59,
-      id:2,
-    },
-    {
-      title:"Third Video",
-      rating:5,
-      comments:2,
-      createdAt:"2 minutes ago",
-      views:59,
-      id:3,
-    }
-  ]
-  res.render('home', { pageTitle: 'Home', videos })
-}
+  res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => {
-  return res.render('watch', { pageTitle: 'Watch Video' })
-}
+  const { id } = req.params;
+  console.log("Show video ", id);
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 export const edit = (req, res) =>
-  res.render('edit', { pageTitle: 'Edit Video' })
-export const search = (req, res) => res.send('Search')
-export const upload = (req, res) => res.send('upload')
-export const deleteVideo = (req, res) => res.send('Delete Video')
+  res.render("edit", { pageTitle: "Edit Video" });
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("upload");
+export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
-import express from 'express'
-import morgan from 'morgan'
-import globalRouter from './routers/globalRouter'
-import userRouter from './routers/userRouter'
-import videoRouter from './routers/videoRouter'
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
-const PORT = 4001
-const app = express()
-const logger = morgan('dev')
+const PORT = 4000;
+const app = express();
+const logger = morgan("dev");
 
-app.set("view engine","pug");
-app.set("views",process.cwd() + "/src/views")
-app.use(logger)
-app.use('/videos', videoRouter)
-app.use('/users', userRouter)
-app.use('/', globalRouter)
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+app.use("/", globalRouter);
 
 app.listen(PORT, () =>
-  console.log('Server listening on port http://localhost:4000'),
-)
+  console.log("Server listening on port http://localhost:4000")
+);
