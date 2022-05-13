aba2cebf20f44aaf0f733e7f6dd0c88825bbc835
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-05-13 16:58:23 +0900

    lec 6.3

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,15 @@
 import express from 'express'
-import { see, getEdit, upload, deleteVideo ,postEdit} from '../controllers/videoController'
+import {
+  see,
+  getEdit,
+  upload,
+  deleteVideo,
+  postEdit,
+} from '../controllers/videoController'
 
 const videoRouter = express.Router()
 
-
 videoRouter.get('/:id(\\d+)', see)
-videoRouter.get('/:id(\\d+)/edit', getEdit)
-videoRouter.post('/:id(\\d+)/edit',postEdit )
-
+videoRouter.route('/:id(\\d+)/edit').get(getEdit).post(postEdit)
 
 export default videoRouter
