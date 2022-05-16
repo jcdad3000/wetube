5cf836cfc6715769457fb8a95e59145876cffc2f
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-05-16 10:45:17 +0900

    lec 6.7

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -48,9 +48,20 @@ export const postEdit = (req, res) => {
 }
 
 export const getUpload = (req, res) => {
+  
   return res.render('upload', { pageTitle: 'Upload Video' })
 }
 
 export const postUpload = (req, res) => {
-  return res.redirect('/')
+  const {title} = req.body;
+  const newVideo={
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: 'just now',
+    views: 0,
+    id: videos.length+1,
+  };
+  videos.push(newVideo)
+  return res.redirect("/");
 }
