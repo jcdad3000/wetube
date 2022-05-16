2b8f9a5a5013c8b10aff7f1698869b354d8f6fab
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-05-16 16:42:07 +0900

    lec 6.11

--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
-import "./db.js";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
-const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
@@ -18,6 +15,4 @@ app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 app.use("/", globalRouter);
 
-app.listen(PORT, () =>
-  console.log(`Server listening on port http://localhost:${PORT}`)
-);
+export default app;
