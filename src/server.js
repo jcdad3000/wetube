9c7903f41b1ab547a382cfeb2ecded02fffb1e02
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-05-16 14:21:27 +0900

    lec 6.9

--- src/server.js
+++ src/server.js
@@ -1,21 +1,23 @@
-import express from 'express'
-import morgan from 'morgan'
-import globalRouter from './routers/globalRouter'
-import userRouter from './routers/userRouter'
-import videoRouter from './routers/videoRouter'
+import "./db.js";
 
-const PORT = 4000
-const app = express()
-const logger = morgan('dev')
+import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
-app.set('view engine', 'pug')
-app.set('views', process.cwd() + '/src/views')
-app.use(logger)
-app.use(express.urlencoded({ extended: true }))
-app.use('/videos', videoRouter)
-app.use('/users', userRouter)
-app.use('/', globalRouter)
+const PORT = 4000;
+const app = express();
+const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+app.use("/", globalRouter);
 
 app.listen(PORT, () =>
-  console.log('Server listening on port http://localhost:4000'),
-)
+  console.log(`Server listening on port http://localhost:${PORT}`)
+);
