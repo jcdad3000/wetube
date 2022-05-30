9b247833a28b25d4f685fa88893df25c10615d30
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-05-30 10:00:56 +0900

    lec 6.17

--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ import "./db.js";
 import "./models/Video";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = 4001;
 
 app.listen(PORT, () =>
   console.log(`Server listening on port http://localhost:${PORT}`)
