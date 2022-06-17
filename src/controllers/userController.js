58a9ffb162a08ae1c87f2d208a271082ce09a42f
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-06-17 11:34:25 +0900

    lec 7.5

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,18 +11,38 @@ export const postJoin = async (req,res) =>{
     if (exists){
         return res.status(400).render("join",{pageTitle,errorMessage:"This username/email is already exist."})
     }
+    try{
+        await User.create({
+            name,
+            username,
+            email,
+            password,
+            location,
+        })
+        return res.redirect("/login");
+    } catch(error){
+        return res.status(400).render("join",{
+            pageTitle:"Join",
+            errorMessage:error._message,
+        })
+    }
+
+}
+
+export const getLogin = (req, res) => res.render('login',{pageTitle:"Login"})
+
+export const postLogin =async (req,res) =>{
+    const {username,password} = req.body;
+    const exists = await User.exists({username})
+
+    if(!exists){
+        return res.status(400).render("login",{pageTitle:"Login",errorMessage:"An Account with this username doesn't exists"})
+    }
     
-    await User.create({
-        name,
-        username,
-        email,
-        password,
-        location,
-    })
-    return res.redirect("/login");
+    res.end();
 }
 export const edit = (req, res) => res.send('Edit User')
 export const remove = (req, res) => res.send('Remove User')
-export const login = (req, res) => res.send('Login User')
+
 export const logout = (req, res) => res.send('Log Out')
 export const see = (req, res) => res.send('See')
