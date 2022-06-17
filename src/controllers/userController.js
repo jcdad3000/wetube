8014d12b199f0e057e0c9392d2da3bec10e65bab
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-06-17 11:19:25 +0900

    lec 7.4

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,11 +5,11 @@ export const postJoin = async (req,res) =>{
     const {name,username,email,password,password2,location} = req.body;
     const pageTitle = "Join"
     if(password !==password2){
-        return res.render("join",{pageTitle,errorMessage:"password confirmation does not matched"})
+        return res.status(400).render("join",{pageTitle,errorMessage:"password confirmation does not matched"})
     }
     const exists = await User.exists({$or: [{username},{email}]});
     if (exists){
-        return res.render("join",{pageTitle,errorMessage:"This username/email is already exist."})
+        return res.status(400).render("join",{pageTitle,errorMessage:"This username/email is already exist."})
     }
     
     await User.create({
