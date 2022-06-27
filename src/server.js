f6fb7d2b14edf07d67714ee46ea4e0781752d847
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-06-27 14:20:16 +0900

    lec 7.15

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
@@ -16,10 +17,10 @@ app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave:false,
-    saveUninitialized:false,
-    store:MongoStore.create({mongoUrl:"mongodb://127.0.0.1:27017/wetube"}),
+    saveUninitialized:false,    
+    store:MongoStore.create({mongoUrl:process.env.DB_URL}),
 }))
 
 app.use(localsMiddleware)
