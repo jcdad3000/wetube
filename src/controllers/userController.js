596afc6d4b78a483f198d411e682fd050581169c
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-06-28 16:15:29 +0900

    lec 7.19

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,95 +1,118 @@
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
-export const getJoin = (req, res) => res.render("join",{pageTitle:"Join"})
-export const postJoin = async (req,res) =>{
-    const {name,username,email,password,password2,location} = req.body;
-    const pageTitle = "Join"
-    if(password !==password2){
-        return res.status(400).render("join",{pageTitle,errorMessage:"password confirmation does not matched"})
-    }
-    const exists = await User.exists({$or: [{username},{email}]});
-    if (exists){
-        return res.status(400).render("join",{pageTitle,errorMessage:"This username/email is already exist."})
-    }
-    try{
-        await User.create({
-            name,
-            username,
-            email,
-            password,
-            location,
-        })
-        return res.redirect("/login");
-    } catch(error){
-        return res.status(400).render("join",{
-            pageTitle:"Join",
-            errorMessage:error._message,
-        })
-    }
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "password confirmation does not matched",
+    });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already exist.",
+    });
+  }
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "Join",
+      errorMessage: error._message,
+    });
+  }
+};
 
-}
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
 
-export const getLogin = (req, res) => res.render('login',{pageTitle:"Login"})
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
 
-export const postLogin =async (req,res) =>{
-    const {username,password} = req.body;
-    const pageTitle = "Login"
-    const user = await User.findOne({username});
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An Account with this username doesn't exists",
+    });
+  }
 
-    if(!user){
-        return res.status(400).render("login",{pageTitle,errorMessage:"An Account with this username doesn't exists"})
-    }
-    
-    const ok = await bcrypt.compare(password,user.password)
+  const ok = await bcrypt.compare(password, user.password);
 
-    if(!ok){
-        return res.status(400).render("login",{pageTitle,errorMessage:"Wrong Password"})
-    }
+  if (!ok) {
+    return res
+      .status(400)
+      .render("login", { pageTitle, errorMessage: "Wrong Password" });
+  }
 
-    req.session.loggedIn = true;
-    req.session.user = user;
-    
-    return res.redirect("/");
-}
+  req.session.loggedIn = true;
+  req.session.user = user;
 
-export const startGithubLogin =(req,res)=>{
-   //https://github.com/login/oauth/authorize?client_id=58c630158a47449e977c&allow_signup=false&scope=user:email
-    const baseUrl = `https://github.com/login/oauth/authorize`;
-    const config = {
-        client_id :process.env.GH_CLIENT,
-        allow_signup:false,
-        scope : "read:user user:email",
-    }
-    const params = new URLSearchParams(config).toString();
-    const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect("/");
+};
 
-    return res.redirect(finalUrl);
+export const startGithubLogin = (req, res) => {
+  //https://github.com/login/oauth/authorize?client_id=58c630158a47449e977c&allow_signup=false&scope=user:email
+  const baseUrl = `https://github.com/login/oauth/authorize`;
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
 
-}
+  return res.redirect(finalUrl);
+};
 
-export const finishGithubLogin =async (req,res) =>{
-    const baseUrl="https://github.com/login/oauth/access_token"
-    const config={
-        client_id:process.env.GH_CLIENT,
-        client_secret:process.env.GH_SECRET,
-        code:req.query.code,
-    }
-    const params = new URLSearchParams(config).toString();    
-    const finalURL = `${baseUrl}?${params}`
-    const data = await fetch(finalURL,{
-        method:"POST",
-        headers:{
-            Accept:"application/json",
-        },
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalURL = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalURL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
     })
+  ).json();
 
-    const json = await data.json()
-    console.log(json)
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const userRequest = await (
+      await fetch("https://api.github.com/user", {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(userRequest);
+  } else {
+    return res.redirect("/login");
+  }
+};
+export const edit = (req, res) => res.send("Edit User");
+export const remove = (req, res) => res.send("Remove User");
 
-}
-export const edit = (req, res) => res.send('Edit User')
-export const remove = (req, res) => res.send('Remove User')
-
-export const logout = (req, res) => res.send('Log Out')
-export const see = (req, res) => res.send('See')
+export const logout = (req, res) => res.send("Log Out");
+export const see = (req, res) => res.send("See");
