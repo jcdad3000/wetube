b55f720bee59aaf10697438f73057b1dd255b493
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-07-05 09:34:10 +0900

    lec 8.7

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -152,12 +152,12 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
     file,
   } = req;
-  console.log(file);
+
   const checkUser = await User.findById(_id);
   if (checkUser.email != email) {
     const exists = await User.exists({ $or: [{ email }] });
@@ -183,6 +183,7 @@ export const postEdit = async (req, res) => {
   const updateUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
