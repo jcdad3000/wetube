6879bec5a3c8f12fcd8f5833d4acf552d313ec48
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-07-06 13:42:13 +0900

    homework 5

--- homework/homework5/src/movieController.js
+++ homework/homework5/src/movieController.js
@@ -5,6 +5,33 @@ import {
   getMovieByMinimumYear
 } from "./db";
 
-export const home = (req, res) => {};
-export const movieDetail = (req, res) => {};
-export const filterMovie = (req, res) => {};
+export const home = async (req, res) => {
+  try {
+    const movies = getMovies();
+    res.render("home", { pageTitle: "Movies!", movies });
+  } catch (error) {
+    return res.render("server-error", { error });
+  }
+};
+export const movieDetail = (req, res) => {
+  const { id } = req.params;
+  const movie = getMovieById(id);
+  return res.render("movie", { pageTitle: movie.title, movie });
+};
+
+export const filterMovie = (req, res) => {
+  const { year, rating } = req.query;
+  if (year) {
+    const movies = getMovieByMinimumYear(year);
+    return res.render("filter", { pageTitle: "Search by Year", movies, year });
+  }
+  if (rating) {
+    const movies = getMovieByMinimumRating(rating);
+    return res.render("filter", {
+      pageTitle: "Search by Rating",
+      movies,
+      rating
+    });
+  }
+  return res.redirect("/");
+};
--- homework/homework5/src/movieRouter.js
+++ homework/homework5/src/movieRouter.js
@@ -3,4 +3,8 @@ import { home, movieDetail, filterMovie } from "./movieController";
 
 const movieRouter = express.Router();
 
+movieRouter.get("/", home);
+movieRouter.get("/filter", filterMovie);
+movieRouter.get("/:id", movieDetail);
+
 export default movieRouter;
