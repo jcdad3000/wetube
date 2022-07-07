0af9dd7920ce8b52b8fce608dc5bd8f1938263d9
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-07-07 14:07:34 +0900

    lec 8.13

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -233,16 +233,13 @@ export const postChangePassword = async (req, res) => {
 export const see = async (req, res) => {
   const { id } = req.params;
 
-  const user = await User.findById(id);
+  const user = await User.findById(id).populate("videos");
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User Not Found." });
   }
 
-  const videos = await Video.find({ owner: user._id });
-  console.log(videos);
   return res.render("users/profile", {
     pageTitle: `${user.name}'s Profile`,
     user,
-    videos,
   });
 };
