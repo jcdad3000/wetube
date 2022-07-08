13eb30eacc8ac0c310a2f3e8045cb8a05ce4d59d
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-07-08 13:46:38 +0900

    lec 9.3

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: "development",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "assests", "js"),
+    path: path.resolve(__dirname, "assets", "js"),
   },
   module: {
     rules: [
