e8d31ecc4fde8777266fc53fc4c3172fa8da72cf
jcdad3000 <31876648+jcdad3000@users.noreply.github.com>
2022-07-22 14:07:52 +0900

    lec 16.1

--- src/init.js
+++ src/init.js
@@ -1,7 +1,9 @@
-import "dotenv/config"
+import "dotenv/config";
 import "./db.js";
 import "./models/Video";
-import "./models/User"
+import "./models/User";
+import "./models/Comment";
+
 import app from "./server";
 
 const PORT = 4000;
